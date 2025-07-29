10f5702fa090988d15d841d0e8467ce1e2c77064
Nguyễn Việt Quốc An <nguyenan.study@gmail.com>
2025-07-29 12:50:02 +0700

    a1

--- main.cc
+++ main.cc
@@ -10,14 +10,18 @@ using namespace chrono;
 using namespace this_thread;
 
 struct Process {
-    int id;
-    int arv;
-    int cpu;
-    int remaining;
+    uint32_t id;
+    uint32_t arv;
+    uint32_t cpu;
+    uint32_t remaining;
 };
 
+void sleep(const uint32_t ms) noexcept {
+    sleep_for(milliseconds(ms));
+}
+
 int main() noexcept {
-    constexpr int quantum = 2, n = 4;
+    constexpr uint32_t quantum = 2, n = 4;
     vector<Process> processes = {
         {1, 0, 12, 12},
         {2, 1, 3, 3},
@@ -26,29 +30,29 @@ int main() noexcept {
         // {5, 4, 2, 2}
     };
 
-    queue<int> rq;
-    vector<int> completion_time(n, 0);
+    queue<uint32_t> rq;
+    vector<uint32_t> completion_time(n);
 
-    int current_time = 0, done = 0, idx = 0;
+    uint32_t now = 0, done = 0, idx = 0;
     while(done < n) {
-        while(idx < n && processes[idx].arv <= current_time) {
+        while(idx < n && processes[idx].arv <= now) {
             rq.push(idx);
             ++idx;
         }
 
         if(!rq.empty()) {
-            const int pidx = rq.front();
+            const uint32_t pidx = rq.front();
             rq.pop();
             Process& proc = processes[pidx];
 
-            const int time_to_run = min(quantum, proc.remaining);
-            current_time += time_to_run;
+            const uint32_t time_to_run = min(quantum, proc.remaining);
+            now += time_to_run;
             proc.remaining -= time_to_run;
 
-            sleep_for(milliseconds(time_to_run * 250));
+            sleep(time_to_run * 250);
             cout << "Tien trinh " << proc.id << " vua chay duoc " << time_to_run / 4.0 << "s\n";
 
-            while(idx < n && processes[idx].arv <= current_time) {
+            while(idx < n && processes[idx].arv <= now) {
                 rq.push(idx);
                 idx++;
             }
@@ -57,31 +61,31 @@ int main() noexcept {
                 rq.push(pidx);
             }
             else {
-            	  cout << "Tien trinh " << proc.id << " da chay xong\n";
-                completion_time[pidx] = current_time;
+                cout << "Tien trinh " << proc.id << " da chay xong\n";
+                completion_time[pidx] = now;
                 ++done;
             }
         }
         else {
             if(idx < n)
-                current_time = processes[idx].arv;
+                now = processes[idx].arv;
         }
     }
 
     cout << "\n--------------------------------------------------\n" << std::left
-    	 << setw(14) << "ID Tien trinh" << setw(22) << "| TG luu lai he thong" << "| TG cho\n"
-    	 << "--------------|---------------------|-------------\n";
+         << setw(14) << "ID Tien trinh" << setw(22) << "| TG luu lai he thong" << "| TG cho\n"
+         << "--------------|---------------------|-------------\n";
 
-    double total_waiting_time = 0.0;
-    for(size_t i = 0; i < n; ++i) {
-        const int alive = completion_time[i] - processes[i].arv;
-        const int wait = alive - processes[i].cpu;
-        total_waiting_time += wait;
+    double sum = 0.0;
+    for(uint32_t i = 0; i < n; ++i) {
+        const uint32_t alive = completion_time[i] - processes[i].arv;
+        const uint32_t wait = alive - processes[i].cpu;
+        sum += wait;
 
         cout << std::left << setw(14) << processes[i].id
              << "| " << setw(20) << alive
              << "| " << wait << endl;
     }
 
-    cout << "\nThoi gian cho trung binh: " << fixed << setprecision(2) << total_waiting_time / n << endl;
+    cout << "\nThoi gian cho trung binh: " << fixed << setprecision(2) << sum / n << endl;
 }
